8dc145ef0aa2fa33ec8edfe3c2d52892dcd508b9
Dipu Sharma <sdipu231@gmail.com>
2023-01-15 12:37:07 +0530

    commit

--- components/Item.jsx
+++ components/Item.jsx
@@ -1,9 +1,10 @@
 import { Paper, Button } from '@mui/material';
+import Image from 'next/image';
 
 export default function Item({ item }) {
     return (
         <Paper>
-            <img src={item.imageUrl} alt={item.title} style={{ width: "100%", height: "60vh" }} />
+            <Image src={item.imageUrl} alt={item.title} style={{ width: "100%", height: "60vh" }} />
 
             <div className='descriptions' style={{ display:"flex", justifyContent:"center", alignItems:"center", flexDirection:"column"}}>
                 <h2>{item.body}</h2>
--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,6 @@ import Container from "@mui/material/Container";
 import Carousel from "../components/Carousel";
 
 export default function Home() {
-  const ImageList = [
-    <img src="https://media.geeksforgeeks.org/wp-content/uploads/20211213172224/1.png" />,
-    <img src="https://media.geeksforgeeks.org/wp-content/uploads/20211213172225/2.png" />,
-    <img src="https://media.geeksforgeeks.org/wp-content/uploads/20211213172226/3.png" />,
-    <img src="https://media.geeksforgeeks.org/wp-content/uploads/20211213172227/4.png" />,
-    <img src="https://media.geeksforgeeks.org/wp-content/uploads/20211213172229/5.png" />,
-  ];
   return (
     <>
       <CssBaseline />
